62675cf26380aeb3300f3097c6a7817946b72cd0
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-08-20 13:53:48 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -1,9 +1,9 @@
 from pyrogram import Client as c
 
-API_ID = input("\7649193\n > ")
-API_HASH = input("\f3a395f2561b2e115658a058fb798860\n > ")
+API_ID = input("7649193")
+API_HASH = input("f3a395f2561b2e115658a058fb798860")
 
-print("\n\+14433530435\n\n")
+print("+14433530435")
 
 i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 
